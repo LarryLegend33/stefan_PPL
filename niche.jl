347e4dc9e4ccdeb8f3b914e1b8bb2716ba6c44f6
larrylegend33 <andrewdbolton@fas.harvard.edu>
2023-02-22 20:39:13 -0500

    rejection sampling implemented

---
+++ niche.jl
@@ -0,0 +1,62 @@
+using Gen
+using GLMakie
+
+
+@gen function draw_personality_type()
+    personality_type = { :ptype } ~ categorical([.2, .6, .2])
+    yell = { * } ~ generate_behavior_discrete(personality_type)
+    return yell
+end
+
+
+@gen function generate_behavior_discrete(ptype::Int)
+    if ptype == 1
+        yell_level = { :ylev } ~ categorical([.5, .15, .15, .05, .05, .025, .025, .025, 0.025])
+    elseif ptype == 2
+        yell_level = { :ylev } ~ categorical([.025, .025, .05, .15, .5, .15, .05, .025, .025])
+    elseif ptype == 3
+        yell_level = { :ylev } ~  categorical([.025, .025, .025, .025, .05, .05, .15, .15, .5])
+    end
+end
+
+@dist function labeled_cat(labels, probs)
+    index = categorical(probs)
+    labels[index]
+end
+
+
+function niche_rejection_sampler(obs_ylev::Int, num_samples::Int)
+    i = 0
+    inference_results = []
+    while  i < num_samples
+        tr = simulate(draw_personality_type, ())
+        ch = get_choices(tr)
+        yell_level = ch[:ylev]
+        if yell_level == obs_ylev
+            push!(inference_results, ch[:ptype])
+            i += 1
+            println(i)
+        end
+    end
+    fig = Figure(resolution=(1000, 1000))
+    ax = Axis(fig[1, 1])
+    hist!(ax, inference_results, xticks=1:1:3, xlabel="Personality Type")
+    display(fig)
+    return inference_results
+end
+
+    
+        
+
+
+
+
+# @gen function generate_behavior_continuous(ptype::Int)
+#     if ptype == 1
+#         yell_level = { :ylev } ~ beta(1, 5)
+#     elseif ptype == 2
+#         yell_level = { :ylev } ~ beta(2, 2)
+#     elseif ptype == 3
+#         yell_level = { :ylev } ~ beta(5, 1)
+#     end
+# end
